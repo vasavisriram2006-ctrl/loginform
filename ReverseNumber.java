b78c00c85e8c98e5688b827c98164287b25fc8a5
vasavisriram <vasavisriram2006@gmail.com>
2026-02-18 10:47:28 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,16 @@
+class ReverseNumber
+{
+Public static void main(String args[])
+{
+int n=Integer.parseInt(args[0])
+int rev=0;
+int r;
+while(n>0)
+{
+r=n%10;
+rev=(rev*10)+r;
+n=n/10;
+}
+System.out.println("reverse number:"+rev);
+}
+}
